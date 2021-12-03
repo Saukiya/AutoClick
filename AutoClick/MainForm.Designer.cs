3972b93e5d25dfd4eb8d02eab34250888e9ebd83
Saukiya <Saukiya@qq.com>
2021-12-03 16:12:51 +0800

    Initial commit

---
+++ AutoClick/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AutoClick
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_click = new System.Windows.Forms.Button();
+            this.btn_move = new System.Windows.Forms.Button();
+            this.Logger = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_click
+            // 
+            this.btn_click.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btn_click.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_click.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (134)));
+            this.btn_click.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_click.Location = new System.Drawing.Point(140, 20);
+            this.btn_click.Name = "btn_click";
+            this.btn_click.Size = new System.Drawing.Size(100, 30);
+            this.btn_click.TabIndex = 0;
+            this.btn_click.Text = "鼠标连点";
+            this.btn_click.UseVisualStyleBackColor = false;
+            // 
+            // btn_move
+            // 
+            this.btn_move.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btn_move.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_move.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (134)));
+            this.btn_move.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_move.Location = new System.Drawing.Point(20, 20);
+            this.btn_move.Name = "btn_move";
+            this.btn_move.Size = new System.Drawing.Size(100, 30);
+            this.btn_move.TabIndex = 0;
+            this.btn_move.Text = "自动奔跑";
+            this.btn_move.UseVisualStyleBackColor = false;
+            // 
+            // Logger
+            // 
+            this.Logger.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Logger.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Logger.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (134)));
+            this.Logger.ForeColor = System.Drawing.SystemColors.Control;
+            this.Logger.Location = new System.Drawing.Point(20, 70);
+            this.Logger.Name = "Logger";
+            this.Logger.Size = new System.Drawing.Size(220, 30);
+            this.Logger.TabIndex = 2;
+            this.Logger.Text = "使用热键连点鼠标";
+            this.Logger.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(264, 121);
+            this.Controls.Add(this.Logger);
+            this.Controls.Add(this.btn_move);
+            this.Controls.Add(this.btn_click);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "原神解放器";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btn_click;
+
+        private System.Windows.Forms.Button btn_move;
+
+        private System.Windows.Forms.Label Logger;
+
+
+        #endregion
+  }
+}
---
+++ AutoClick/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoClick
+{
+    public partial class MainForm : Form
+    {
+
+        public static MainForm inst;
+        
+        public Keys key_click = Keys.None;
+        
+        public Keys key_move = Keys.None;
+
+        public bool boo_click;
+
+        public bool boo_move;
+
+        [DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [DllImport("user32")]
+        private static extern int keybd_event(byte bVK, byte bScan, int dwFlags, int dwExtraInfo);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        
+        //模拟键盘按下 
+        const int KEY_DOWN = 0x0000;
+        //模拟键盘拾起 
+        const int KEY_UP = 0x0002;
+        //模拟鼠标左键按下 
+        const int MOUSE_L_DOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSE_L_UP = 0x0004;
+        
+        public MainForm()
+        {
+            inst = this;
+            InitializeComponent();
+            btn_click.KeyUp += (sender, args) =>
+            {
+                key_click = args.KeyCode;
+                btn_click.Text = "连点-" + key_click.ToString();
+                boo_click = false;
+                ActiveControl = Logger;
+            };
+
+            btn_click.Click += (sender, args) =>
+            {
+                key_click = Keys.None;
+                btn_click.Text = "设置热键";
+                boo_click = false;
+            };
+            
+            btn_move.KeyUp += (sender, args) =>
+            {
+                key_move = args.KeyCode;
+                btn_move.Text = "移动-" + key_move.ToString();
+                boo_move = false;
+                ActiveControl = Logger;
+            };
+
+            btn_move.Click += (sender, args) =>
+            {
+                key_move = Keys.None;
+                btn_move.Text = "设置热键";
+                boo_move = false;
+            };
+            
+            ActiveControl = Logger;
+            
+            FormClosed += (sender, args) => 
+            {
+                Environment.Exit(0);
+            };
+
+            new Thread(() => 
+            {
+                while (true)
+                {
+                    if (boo_click && GetForegroundWindowTitle().Equals("原神")) 
+                    {
+                        mouse_event(MOUSE_L_DOWN, 0, 0, 0, 0);
+                        mouse_event(MOUSE_L_UP, 0, 0, 0, 0);
+                    }
+                    Thread.Sleep(100);
+                }
+            }).Start();
+            
+            KeyboardHook keyboardHook = new KeyboardHook();
+            keyboardHook.KeyDownEvent += (sender, args) =>
+            {
+                if (args.KeyCode == key_click && GetForegroundWindowTitle().Equals("原神"))
+                {
+                    boo_click = true;
+                    Log("鼠标连点已开启");
+                } else if (args.KeyCode == key_move && GetForegroundWindowTitle().Equals("原神"))
+                {
+                    if (!boo_move)
+                    {
+                        new Thread(() =>
+                        {
+                            keybd_event(0x10, 0, KEY_DOWN, 0);
+                            Thread.Sleep(3000);
+                            keybd_event(0x10, 0, KEY_UP, 0);
+                        }).Start();
+                        keybd_event((byte) Keys.W, 0, KEY_DOWN, 0);
+                        Log("自动奔跑已开启");
+                        boo_move = true;
+                    }
+                    else
+                    {
+                        keybd_event((byte) Keys.W, 0, KEY_UP, 0);
+                        Log("自动奔跑已关闭");
+                        boo_move = false;
+                    }
+                } else if (args.KeyCode == Keys.W && boo_move)
+                {
+                    Log("自动奔跑已关闭");
+                    boo_move = false;
+                }
+
+            };
+            keyboardHook.KeyUpEvent += (sender, args) =>
+            {
+                if (boo_click && args.KeyCode == key_click)
+                {
+                    boo_click = false;
+                    Log("鼠标连点已关闭");
+                }else if (args.KeyCode == Keys.W && boo_move && GetForegroundWindowTitle().Equals("原神"))
+                {
+                    boo_move = false;
+                    keybd_event((byte) Keys.W, 0, KEY_DOWN, 0);
+                }
+            };
+            keyboardHook.Start();
+        }
+
+        public static void Log(string log)
+        {
+            inst.Logger.Text = log;
+        }
+
+        public static String GetForegroundWindowTitle()
+        {
+            IntPtr intPtr = GetForegroundWindow();
+            int length = GetWindowTextLength(intPtr) * 2;
+            StringBuilder sb = new StringBuilder(length);
+            GetWindowText(intPtr, sb, length);
+            return sb.ToString();
+        }
+    }
+}
